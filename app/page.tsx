65fb82c843aa3312be52b855d19d7396e211bf01
prashant998132-cpu <prashant998132@gmail.com>
2026-02-25 09:06:03 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+import ChatInterface from '@/components/ChatInterface';
+
+export default function Home() {
+  return (
+    <main className="h-screen overflow-hidden">
+      <ChatInterface />
+    </main>
+  );
+}
